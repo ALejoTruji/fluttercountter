41dbb55369c157f17409c56898a5801a4fa5a6da
ALejoTruji <alejandrotrujipz@gmail.com>
2025-11-05 21:20:23 -0500

    counter Flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:counter/presentation/screens/CounterScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      theme: ThemeData(colorSchemeSeed: Colors.blue),
+      home: CounterScreen(),
+    );
+  }
+}
